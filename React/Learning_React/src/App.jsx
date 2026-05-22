d04b5717b1d84df2dea819fafc6df9b15446680f
MrVinayakGupta <163561684+MrVinayakGupta@users.noreply.github.com>
2026-05-22 16:47:47 +0530

    Create App.jsx

---
+++ React/Learning_React/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Section1 from "./components/section1.jsx"; 
+import Section2 from "./components/section2.jsx"; 
+
+function App() {
+  return (
+    <>
+      <Section1 />
+      <hr />
+      <Section2 />
+    </>
+  );
+}
+
+export default App;
